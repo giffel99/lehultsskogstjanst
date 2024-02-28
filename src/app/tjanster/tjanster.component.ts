115466e239c8d5756627bebff6fb30255aa8c2f5
Pär Aronsson <per_a99@hotmail.com>
2024-02-28 21:16:06 +0100

    site ready for deploy

--- src/app/tjanster/tjanster.component.ts
+++ src/app/tjanster/tjanster.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tjanster',
   templateUrl: './tjanster.component.html',
-  styleUrls: ['./tjanster.component.scss']
+  styleUrls: ['./tjanster.component.scss'],
 })
 export class TjansterComponent {
-
+  services: string[] = [
+    'Vatten och avlopp',
+    'Dammar',
+    'Dikning',
+    'Skogsvägar',
+    'Tomtplantering',
+    'Trädgårdsmurar',
+    'Husgrunder',
+    'Dränering',
+  ];
 }
